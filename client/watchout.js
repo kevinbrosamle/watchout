c1326fa3c29f2e654dac0a3499320df5a5be23d0
kevinbrosamle <kevin.brosamle@gmail.com>
2016-08-06 12:29:14 -0700

    implemented draggable player icon

--- client/watchout.js
+++ client/watchout.js
@@ -2,6 +2,8 @@
 // difficulty level can set amount of asteroids
 var moveTime = 1500;
 var numOfAsteroids = 20;
+var boxWidth = 600;  
+var boxHeight = 600;
 var dataSet = [];
 
 for (var i = 0; i < numOfAsteroids; i++) {
@@ -11,7 +13,8 @@ for (var i = 0; i < numOfAsteroids; i++) {
 // create asteroids
 var svg = d3.select('svg');
 var circle = svg.selectAll('circle').data(dataSet);
-var circleEnter = circle.enter().append('circle');
+var circleEnter = circle.enter().append('svg:circle');
+circleEnter.attr('class', 'circle');
 circleEnter.attr('fill', 'blue');
 circleEnter.attr('r', function(d) { return d; });
 circleEnter.attr('cx', function() { return Math.random() * 600; });
@@ -27,3 +30,30 @@ var randomizeAsteroids = function() {
 
 // move asteroids every 1.5 seconds
 setInterval(function() { randomizeAsteroids(); }, moveTime);
+
+// make player dot
+var drag = d3.behavior.drag()  
+             .on('dragstart', function() { player.style('fill', 'red'); })
+             .on('drag', function() { player.attr('cx', d3.event.x).attr('cy', d3.event.y); })
+             .on('dragend', function() { player.style('fill', 'black'); });
+
+var player = svg.selectAll('.draggableCircle')  
+                .data([{ x: (boxWidth / 2), y: (boxHeight / 2), r: 10 }])
+                .enter()
+                .append('svg:circle')
+                .attr('class', 'draggableCircle')
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('r', function(d) { return d.r; })
+                .call(drag)
+                .style('fill', 'black');
+
+
+
+// check if mouse is touching any asteroids
+$( document ).ready(function() {
+  $( '.circle' ).mouseenter(function() {
+    console.log('test');
+  });
+});
+
